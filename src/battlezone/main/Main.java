7b0bed33021ff4734c2311ab366cdf460062fad0
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-13 09:29:42 +0100

    Ładowanie okna

--- src/battlezone/main/Main.java
+++ src/battlezone/main/Main.java
@@ -1,9 +1,25 @@
 package battlezone.main;
 
+import maths.Vector2;
+import renderer.Camera;
+import renderer.Window;
 import util.Console;
+import renderer.Renderer;
+import engine.Engine;
 
 public class Main {
+  public static final int WIDTH=1920, HEIGHT=1080;
+  public static final String TITLE = "Battlezone";
   public static void main(String[] args) {
-    Console.log("Hello world!");
+    Camera camera = new Camera();
+    Renderer renderer  = new MainRenderer(new Vector2(WIDTH, HEIGHT), camera);
+    Window window = new Window(new Vector2(WIDTH, HEIGHT), TITLE, renderer);
+    renderer.addKeyListener(camera);
+    renderer.addMouseMotionListener(camera);
+    renderer.requestFocus();
+    camera.enableRotationPitch = false;
+    MainLogic mainLogic = new MainLogic();
+    Engine engine = new Engine(renderer, mainLogic);
+    engine.start();
   }
 }
---
+++ src/battlezone/main/MainRenderer.java
@@ -0,0 +1,19 @@
+package battlezone.main;
+
+import maths.Vector2;
+import renderer.Camera;
+import renderer.Renderer;
+
+import java.awt.*;
+
+public class MainRenderer extends Renderer {
+
+
+    public MainRenderer(Vector2 vector2, Camera camera) {
+        super(vector2, camera);
+    }
+
+    public void render(Graphics2D graphics2D) {
+
+    }
+}
