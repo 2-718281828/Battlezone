c53c8acb5392f1671a4afcae8fdbad7eab062fc5
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-15 08:04:40 +0100

    Ładowanie okna

--- src/battlezone/main/MainRenderer.java
+++ src/battlezone/main/MainRenderer.java
@@ -1,19 +1,26 @@
 package battlezone.main;
 
 import maths.Vector2;
-import renderer.Camera;
-import renderer.Renderer;
+import renderer.*;
 
 import java.awt.*;
+import java.io.File;
+import java.util.Locale;
 
 public class MainRenderer extends Renderer {
 
-
+    public Model model;
+    public Triangles triangles;
+    String classPath = getClass().getResource("").getPath() + "/torus.model";
     public MainRenderer(Vector2 vector2, Camera camera) {
         super(vector2, camera);
+
+        triangles = new Triangles();
+        model = LoadModel.loadModel(new File(classPath), Color.red, this, camera);
+        model.init(triangles);
     }
 
     public void render(Graphics2D graphics2D) {
-
+        triangles.render(graphics2D);
     }
 }
