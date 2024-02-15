d75da36b8b127541d8885c2d10db086c32068869
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-15 08:15:04 +0100

    Ładowanie okna

--- src/battlezone/main/MainLogic.java
+++ src/battlezone/main/MainLogic.java
@@ -1,16 +1,25 @@
 package battlezone.main;
 
 import engine.Logic;
+import entity.EntityHandler;
+import maths.Vector3;
 import renderer.Camera;
+import renderer.LoadModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class MainLogic implements Logic {
 
-    Camera camera;
-
-    public MainLogic(Camera camera) {
+    public Camera camera;
+    public EntityHandler entityHandler;
+    String classPath=getClass().getResource("").getPath();
+    public MainLogic(Camera camera){
         this.camera = camera;
+        entityHandler = new EntityHandler();
+        entityHandler.entities.add(new Torus(LoadModel.loadModel(new File(classPath + "/torus.mode"), Color.green, camera.renderer, camera), new Vector3(0,0,0), entityHandler));
     }
-
     public void update() {
         camera.update();
     }
