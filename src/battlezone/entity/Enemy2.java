84840b6394120095bd53d5e118cf319fdeed2bc5
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-16 10:14:08 +0100

    Entity

--- src/battlezone/entity/Enemy2.java
+++ src/battlezone/entity/Enemy2.java
@@ -1,21 +1,22 @@
 package battlezone.entity;
-
 import entity.Entity;
 import entity.EntityHandler;
 import maths.Vector3;
 import renderer.Model;
 import util.Console;
-public class Enemy1 extends Entity {
-    public ID id = ID.Enemy1;
-    public Enemy1(Model model, Vector3 position, EntityHandler entityHandler){
+
+public class Enemy2 extends Entity{
+    public ID id = ID.Enemy2;
+    public Enemy2(Model model, Vector3 position, EntityHandler entityHandler){
         super(model, position, entityHandler);
         velocity = new Vector3(0, 0, 0);
     }
     double t = 0;
     public void logic () {
-        velocity.x =((Math.sin(t))/5)*(-1);
+        velocity.x =(Math.sin(t))/5;
         position.add(velocity);
         model.move(velocity);
         t = t+0.1;
     }
 }
+
--- src/battlezone/main/Main.java
+++ src/battlezone/main/Main.java
@@ -8,17 +8,18 @@ import engine.Engine;
 import java.util.Locale;
 
 public class Main {
-  public static final int WIDTH=1920, HEIGHT=1080;                                     //deklaruje rozmiary okna w pikselach
+  public static final int WIDTH=1280, HEIGHT=720;                                     //deklaruje rozmiary okna w pikselach
   public static final String TITLE = "Battlezone";                                     //deklaruje nazwe okna
   public static void main(String[] args) {
     Locale.setDefault(new Locale("en", "US"));
-    Camera camera = new Camera();                                                     //tworzy kamere
+    Camera camera = new Camera();                                                     //tworzy kamere bul bul bul
     Renderer renderer  = new MainRenderer(new Vector2(WIDTH, HEIGHT), camera);
     Window window = new Window(new Vector2(WIDTH, HEIGHT), TITLE, renderer);
     renderer.addKeyListener(camera);                                                   //uruchamia słuchacze
     renderer.addMouseMotionListener(camera);
     renderer.requestFocus();
-    camera.enableRotationYaw = true;                                                  //wyłącza obrótw kamery w pionie
+    camera.enableRotationYaw = true;  //wyłącza obrótw kamery w pionie
+    camera.enableRotationPitch = true;
     MainLogic mainLogic = new MainLogic(camera);                                       //wspomaga kamere
     Engine engine = new Engine(renderer, mainLogic);                                   //obsługuje silnik
     engine.start();
