e701588e4133d4d497e539eef6b8797f42aa25be
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-19 18:31:13 +0100

    Entity

--- src/battlezone/entity/Enemy1.java
+++ src/battlezone/entity/Enemy1.java
@@ -5,6 +5,8 @@ import entity.EntityHandler;
 import maths.Vector3;
 import renderer.Model;
 import util.Console;
+
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 public class Enemy1 extends Entity {
@@ -17,14 +19,21 @@ public class Enemy1 extends Entity {
 
     }
     double t = 0;
+    double speed = 0.1; //ustalamy domyślną prędkość obiektu
+    double alfa = 0; //kąt względem którego obiekt porusza sie wzgłedem osi x
+    double beta = alfa;  //ostatni kąt przed zmianą kierunku
     public void logic () {
         position.add(velocity);
         model.move(velocity);
-        velocity.x = Math.sin(t)/5;
-        velocity.z = Math.cos(t)/5;
-        model.rotate(1, 0.01);
-        t= t+0.01;
-
+        velocity.x=speed*Math.cos(alfa);
+        velocity.z=speed*Math.sin(alfa);   //zarządza prędkością
+        if (alfa!=beta){                          //alfa to jest kąt kierunku ustalany przez nas na bierząco
+            model.rotate(1, -(alfa-beta));      //beta to poprzedni kąt ustawienia modelu
+            beta=alfa;     //dzięki temu model zawsze jest ustawiony w wybranym przez nas kierunku, a nie kręci się
+        }
+        //kąt między osiami x i z patrząc z góry od przodu jest pierwszej ćwiartce układu
+        alfa = t;  //test
+        t=t+0.01;  //test
     }
     Random random = new Random();
     public void AI() {
