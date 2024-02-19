971e545f6ac55fd9775f53812641252c618c037c
bwegrzyn0 <bwegrzyn@duck.com>
2024-02-19 20:34:35 +0100

    m

--- src/battlezone/entity/Enemy1.java
+++ src/battlezone/entity/Enemy1.java
@@ -5,7 +5,6 @@ import entity.EntityHandler;
 import maths.Vector3;
 import renderer.Model;
 import util.Console;
-import java.util.Random;
 public class Enemy1 extends Entity {
     public ID id = ID.Enemy1;
     public Enemy1(Model model, Vector3 position, EntityHandler entityHandler){
