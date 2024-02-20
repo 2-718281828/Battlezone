42cd57311acc9229fc6622ab69f48e29bf2a4a57
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-20 10:30:05 +0100

    sfdjfisuzdnflvisukdhfivyk5shernyudhvdsewg45wyskgfhdsvreysvtrgfheiyrhsdug6fubyrvgdsy6ht87fuvwegrsduktyrgehvujhryo7ieg6SFAe7hsyerfdewg7ycseud7sgrfi

--- src/battlezone/entity/Enemy1.java
+++ src/battlezone/entity/Enemy1.java
@@ -11,17 +11,19 @@ import java.util.Random;
 
 public class Enemy1 extends Entity {
     public ID id = ID.Enemy1;
+    double t = 0;
+    double speed = 0.01; //ustalamy domyślną prędkość obiektu
+    double alfa = 45; //kąt względem którego obiekt porusza sie wzgłedem osi x
+    double beta = alfa;  //ostatni kąt przed zmianą kierunku
     public Enemy1(Model model, Vector3 position, EntityHandler entityHandler){
         super(model, position, entityHandler);
         velocity = new Vector3(0, 0, 0);
         model.rotate(2, Math.PI);
-        model.rotate(1, Math.PI/2);
-
+        model.rotate(1, Math.PI);
+        model.rotate(1, -alfa);
+        model.scale(0.2);
     }
-    double t = 0;
-    double speed = 0.1; //ustalamy domyślną prędkość obiektu
-    double alfa = 0; //kąt względem którego obiekt porusza sie wzgłedem osi x
-    double beta = alfa;  //ostatni kąt przed zmianą kierunku
+
     public void logic () {
         position.add(velocity);
         model.move(velocity);
@@ -32,8 +34,8 @@ public class Enemy1 extends Entity {
             beta=alfa;     //dzięki temu model zawsze jest ustawiony w wybranym przez nas kierunku, a nie kręci się
         }
         //kąt między osiami x i z patrząc z góry od przodu jest pierwszej ćwiartce układu
-        alfa = t;  //test
-        t=t+0.01;  //test
+        //alfa = t;  //test
+        //t=t+0.01;  //test
     }
     Random random = new Random();
     public void AI() {
--- src/battlezone/entity/Point.java
+++ src/battlezone/entity/Point.java
@@ -8,16 +8,8 @@ public class Point extends Entity {
     public ID id = ID.Point;
     public Point(Model model, Vector3 position, EntityHandler entityHandler){
         super(model, position, entityHandler);
-        Vector3 wektor = new Vector3(position);
-        wektor.multiply(-1);
         velocity = new Vector3(0, 0, 0);
-        model.move(wektor);
-        for(int i=0; i<model.triangles.size(); i++){
-            for(int j=0; j<3; j++){
-                model.triangles.get(i).verticies[j].multiply(0.1);
-            }
-        }
-        model.move(position);
+        model.scale(0.1);
     }
     double t = 0;
     public void logic () {
