1f486d7e3c939852fd4d3d7835d15c923b951988
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-21 12:33:06 +0100

    sfdjfisuzdnflvisukdhfivyk5shernyudhvdsewg45wyskgfhdsvreysvtrgfheiyrhsdug6fubyrvgdsy6ht87fuvwegrsduktyrgehvujhryo7ieg6SFAe7hsyerfdewg7ycseud7sgrfi

--- src/battlezone/entity/Enemy1.java
+++ src/battlezone/entity/Enemy1.java
@@ -1,33 +1,42 @@
 package battlezone.entity;
 
+import battlezone.main.KeyHandler;
 import battlezone.main.MainRenderer;
+import com.sun.source.doctree.AttributeTree;
 import entity.Entity;
 import entity.EntityHandler;
 import maths.Vector3;
 import renderer.Camera;
 import renderer.Model;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 public class Enemy1 extends Entity {
     public ID id = ID.Enemy1;
     double lifetime = 0;
+    boolean life = false;
     double speed = 0.1; //ustalamy domyślną prędkość obiektu
     Camera camera;
     public Enemy1(Model model, Vector3 position, EntityHandler entityHandler, Camera camera) {
         super(model, position, entityHandler);
-        model.scale(0.2);
+        //model.rotate(1, 0.5*Math.PI);
         this.camera = camera;
-        velocity = new Vector3(Math.cos(camera.rotation.x)*speed, Math.sin(camera.rotation.x)*speed, 0);
+        velocity = new Vector3(Math.sin(-camera.rotation.x)*speed, 0, Math.cos(-camera.rotation.x)*speed);
+        model.rotate(1, -camera.rotation.x);
     }
 
-    Random random = new Random();
-    double time = System.currentTimeMillis()/1000 ;
     public void logic() {
         lifetime++;
         if (lifetime >= 60 * 5) {
             entityHandler.entities.remove(this);
             model.remove(((MainRenderer) camera.renderer).triangles);
+            life = false;
+            lifetime = 0;
+        }
+        else{
+            life= true;
         }
         position.add(velocity);
         model.move(velocity);
---
+++ src/battlezone/main/KeyHandler.java
@@ -0,0 +1,28 @@
+package battlezone.main;
+
+import battlezone.entity.Enemy1;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class KeyHandler implements KeyListener {
+    boolean spacePressed=false;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (KeyEvent.VK_SPACE == e.getKeyCode()) {
+            spacePressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (KeyEvent.VK_SPACE == e.getKeyCode()) {
+            spacePressed = false;
+        }
+    }
+}
