d43e3377d5f021d5a4f67692be2da387f4b9a779
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-21 09:40:04 +0100

    sfdjfisuzdnflvisukdhfivyk5shernyudhvdsewg45wyskgfhdsvreysvtrgfheiyrhsdug6fubyrvgdsy6ht87fuvwegrsduktyrgehvujhryo7ieg6SFAe7hsyerfdewg7ycseud7sgrfi

--- src/battlezone/entity/Enemy1.java
+++ src/battlezone/entity/Enemy1.java
@@ -1,44 +1,35 @@
 package battlezone.entity;
 
+import battlezone.main.MainRenderer;
 import entity.Entity;
 import entity.EntityHandler;
 import maths.Vector3;
+import renderer.Camera;
 import renderer.Model;
-import util.Console;
 
-import java.awt.event.KeyListener;
 import java.util.Random;
 
 public class Enemy1 extends Entity {
     public ID id = ID.Enemy1;
-    double t = 0;
-    double speed = 0.01; //ustalamy domyślną prędkość obiektu
-    double alfa = 45; //kąt względem którego obiekt porusza sie wzgłedem osi x
-    double beta = alfa;  //ostatni kąt przed zmianą kierunku
-    public Enemy1(Model model, Vector3 position, EntityHandler entityHandler){
+    double lifetime = 0;
+    double speed = 0.1; //ustalamy domyślną prędkość obiektu
+    Camera camera;
+    public Enemy1(Model model, Vector3 position, EntityHandler entityHandler, Camera camera) {
         super(model, position, entityHandler);
-        velocity = new Vector3(0, 0, 0);
-        model.rotate(2, Math.PI);
-        model.rotate(1, Math.PI);
-        model.rotate(1, -alfa);
         model.scale(0.2);
+        this.camera = camera;
+        velocity = new Vector3(Math.cos(camera.rotation.x)*speed, Math.sin(camera.rotation.x)*speed, 0);
     }
 
-    public void logic () {
+    Random random = new Random();
+    double time = System.currentTimeMillis()/1000 ;
+    public void logic() {
+        lifetime++;
+        if (lifetime >= 60 * 5) {
+            entityHandler.entities.remove(this);
+            model.remove(((MainRenderer) camera.renderer).triangles);
+        }
         position.add(velocity);
         model.move(velocity);
-        velocity.x=speed*Math.cos(alfa);
-        velocity.z=speed*Math.sin(alfa);   //zarządza prędkością
-        if (alfa!=beta){                          //alfa to jest kąt kierunku ustalany przez nas na bierząco
-            model.rotate(1, -(alfa-beta));      //beta to poprzedni kąt ustawienia modelu
-            beta=alfa;     //dzięki temu model zawsze jest ustawiony w wybranym przez nas kierunku, a nie kręci się
-        }
-        //kąt między osiami x i z patrząc z góry od przodu jest pierwszej ćwiartce układu
-        //alfa = t;  //test
-        //t=t+0.01;  //test
-    }
-    Random random = new Random();
-    public void AI() {
-    	
     }
 }
