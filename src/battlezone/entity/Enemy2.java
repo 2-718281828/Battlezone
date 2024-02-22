8082a0f2a8311ff8692b83f44efea2d37fb975f1
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-22 13:30:58 +0100

    wróg zatrzymuje sie gdy kamera sie do niego zbliży

--- src/battlezone/entity/Enemy2.java
+++ src/battlezone/entity/Enemy2.java
@@ -16,9 +16,10 @@ public class Enemy2 extends Entity {
     double t = 0;
     Camera camera;
     double speed = 0.03; //ustalamy domyślną prędkość obiektu
-    double alfa=0; //kąt względem którego obiekt porusza sie wzgłedem osi x
-    double delta=0;
-    double beta=alfa; //kąt względem którego obiekt porusza sie wzgłedem osi x
+    double alfa = 0; //kąt względem którego obiekt porusza sie wzgłedem osi x
+    double delta = 0;
+    double beta = alfa; //kąt względem którego obiekt porusza sie wzgłedem osi x
+
     public Enemy2(Model model, Vector3 position, EntityHandler entityHandler, Camera camera) {
         super(model, position, entityHandler);
         velocity = new Vector3(0, 0, 0);
@@ -27,29 +28,38 @@ public class Enemy2 extends Entity {
         this.camera = camera;
 
     }
+
     Random random = new Random();
     long time = 0;
+
     public void logic() {
         time++;
         position.add(velocity);
         model.move(velocity);
-	Vector3 dst = new Vector3(position.x - camera.position.x, 0, position.z-camera.position.z);
-	if (dst.magnitude() < 9){ //to "25" to odległość od gracza ^2
-            //określa odległość Enemy2 od gracza, dzieli logikę na części "walka z graczem" i "jazda losowa"
-	model.rotate(1, -beta);
-	   beta = Math.atan2(dst.z, dst.x);
-	  model.rotate(1, beta); 
+//        Vector3 dst = new Vector3(position.x - camera.position.x, 0, position.z-camera.position.z);
+//	    if (dst.magnitude() < 9){ //to "25" to odległość od gracza ^2
+//            //określa odległość Enemy2 od gracza, dzieli logikę na części "walka z graczem" i "jazda losowa"
+//	        delta=beta;
+//            model.rotate(1, -delta);
+//	        delta = Math.atan2(dst.z, dst.x);
+//            model.rotate(1, delta);
+//        }
+//        else{
+//
+//            }
+//
+//	    }
+        if (time / 60 >= 5) {
+            alfa = random.nextDouble(0.25 * Math.PI);
+            time = 0;
         }
-        else{
-            if (time/60>=5){
-                alfa=random.nextDouble(0.25*Math.PI);
-                model.rotate(1,alfa);
-                time=0;
-                beta+=alfa;
-            }
-	}
-	velocity.x=Math.cos(beta)*speed;
-	velocity.z=Math.sin(beta)*speed;
-
+        if (alfa != beta) {
+            model.rotate(1, -alfa);
+            beta = alfa;
+            delta-=beta;
+        }
+        velocity.x = Math.cos(delta) * speed;
+        velocity.z = Math.sin(delta) * speed;
+        }
     }
-}
+
