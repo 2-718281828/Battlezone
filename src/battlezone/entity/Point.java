86e94dfa2b13603bfedf690772201713c3dd417f
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-23 17:12:00 +0100

    naprawiono błąd, w którym tłem gry jest plik "Andrek.jpg"

--- src/battlezone/entity/Point.java
+++ src/battlezone/entity/Point.java
@@ -4,12 +4,17 @@ import entity.Entity;
 import entity.EntityHandler;
 import maths.Vector3;
 import renderer.Model;
+import util.Console;
+
 public class Point extends Entity {
     public ID id = ID.Point;
     public Point(Model model, Vector3 position, EntityHandler entityHandler){
         super(model, position, entityHandler);
         velocity = new Vector3(0, 0, 0);
         model.scale(0.1);
+        Console.log("x: " +model.rotationAxis.x);
+        Console.log("y: " + model.rotationAxis.y);
+        Console.log("z: " + model.rotationAxis.z);
     }
     double t = 0;
     public void logic () {
