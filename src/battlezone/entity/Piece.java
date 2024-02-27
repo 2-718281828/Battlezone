5f7f1300873e528c9bb38419ae7c16c0f824c0e8
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-27 21:47:12 +0100

    odłamki full edition

--- src/battlezone/entity/Piece.java
+++ src/battlezone/entity/Piece.java
@@ -21,12 +21,15 @@ public class Piece extends Entity {
     Random random = new Random();
     double beta = random.nextDouble(2*Math.PI);
     public void logic() {
-        velocity.x=Math.sin(beta)*0.05;
-        velocity.z=Math.cos(beta)*0.05;
-        velocity.y=Math.sin(alfa)*0.05;
+        velocity.x=Math.sin(beta)*0.1;
+        velocity.z=Math.cos(beta)*0.1;
+        velocity.y=Math.sin(alfa)*0.1;
+        model.rotate(0, 0.1);
+        model.rotate(1, 0.1);
+        model.rotate(2, 0.1);
         position.add(velocity);
         model.move(velocity);
-        alfa -= 0.25*Math.PI/120;
+        alfa -= 0.5*Math.PI/120;
         if (alfa<=-0.25*Math.PI){
             model.remove(((MainRenderer) camera.renderer).triangles);
             entityHandler.entities.remove(this);
