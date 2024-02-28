c26dbf1db30592a55df777ef15f0c2465406f93f
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-28 09:52:01 +0100

    naprawiono błąd w którym Antoni Moskal niszczy laptop udeżając w niego pięścią

--- src/battlezone/entity/ID.java
+++ src/battlezone/entity/ID.java
@@ -1,5 +1,5 @@
 package battlezone.entity;
 
 public enum ID {
-    Point, Tank, Bullet1, Bullet2, SuperTank, UFO, Piece, Tree
+    Point, Tank, Bullet1, Bullet2, SuperTank, UFO, Piece, Tree, Rock
 }
