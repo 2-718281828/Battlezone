609ecf2ae28752ebb4af4b7b957541d4c24f81ce
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-28 23:20:41 +0100

    NAPRAWIŁEM HITBOXY (to wcale nie tak że my ich po prostu nie updatowaliśmy)

--- src/battlezone/entity/UFO.java
+++ src/battlezone/entity/UFO.java
@@ -34,6 +34,7 @@ public class UFO extends Entity {
     long time = 0;
 
     public void logic() {
+        updateHitbox();
         time++;
 	// przemiesczanie
         position.add(velocity);
@@ -50,7 +51,7 @@ public class UFO extends Entity {
         for (int i = 0; i < entityHandler.entities.size(); i++) {
             if (entityHandler.entities.get(i) != this) {
                 if (collision(entityHandler.entities.get(i).hitbox)) {
-                    if(entityHandler.entities.get(i).getClass()==Bullet1.class) {
+                    if(entityHandler.entities.get(i).getClass()==UFO.class) {
                         util.Console.log("Kolizja z pociskiem");
                         entityHandler.entities.get(i).model.remove(((MainRenderer)camera.renderer).triangles);
                         entityHandler.entities.remove(entityHandler.entities.get(i));
