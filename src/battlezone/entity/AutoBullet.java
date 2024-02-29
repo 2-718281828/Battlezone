26dbb1adb240792b66649430aebec8b42fd6270c
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-29 20:25:37 +0100

    ufo naprawione

--- src/battlezone/entity/AutoBullet.java
+++ src/battlezone/entity/AutoBullet.java
@@ -1,20 +1,31 @@
 package battlezone.entity;
 
+import battlezone.main.MainLogic;
+import battlezone.main.MainRenderer;
+import battlezone.main.Sounds;
 import entity.Entity;
 import entity.EntityHandler;
 import maths.Vector3;
 import renderer.Camera;
+import renderer.LoadModel;
 import renderer.Model;
+import util.Console;
+
+import java.awt.*;
+import java.io.File;
 
 public class AutoBullet extends Entity {
     Camera camera;
+
     public AutoBullet(Model model, Vector3 vector3, EntityHandler entityHandler, Camera camera) {
         super(model, vector3, entityHandler);
-        this.camera=camera;
+        this.camera = camera;
     }
+
     double beta;
-    double speed= 0.1;
-    public void logic(){
+    double speed = 0.1;
+
+    public void logic() {
         position.add(velocity);
         model.move(velocity);
         // odległość do kamery
@@ -26,5 +37,26 @@ public class AutoBullet extends Entity {
         model.rotate(1, -beta); // obracamy obiekt tak, aby był obrócony domyślnie
         beta = -Math.atan2(dst.z, dst.x); // kąt pod jakim ma się poruszać czołg aby szedł do kamery
         model.rotate(1, beta); // obracamy obiekt o ten nowy kąt
+        if (Math.abs(position.x - camera.position.x) < 0.5 && Math.abs(position.z - camera.position.z) < 0.5) {
+            MainLogic.health-=3;
+            Sounds.play("/health.wav");
+        }
+        Sounds.play("/autobulletsound.wav");
+
+        for (int i = 0; i < entityHandler.entities.size(); i++) {
+            if (entityHandler.entities.get(i) != this) {
+                if (collision(entityHandler.entities.get(i).hitbox)) {
+                    if(entityHandler.entities.get(i).getClass()==Bullet1.class) {
+                        util.Console.log("Kolizja z pociskiem");
+                        entityHandler.entities.get(i).model.remove(((MainRenderer)camera.renderer).triangles);
+                        entityHandler.entities.remove(entityHandler.entities.get(i));
+                        Sounds.play("/boom.wav");
+                        model.remove(((MainRenderer)camera.renderer).triangles);
+                        entityHandler.entities.remove(this);
+                        MainLogic.score+=50;
+                    }
+                }
+            }
+        }
     }
 }
