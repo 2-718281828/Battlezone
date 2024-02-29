8d0ffcba815ee3c549c8493af4c816f32c2dc902
Szymon4747192847 <152371839+Szymon4747192847@users.noreply.github.com>
2024-02-29 20:02:19 +0100

    ufo do doprkbjhvhgvjhgfcowaniavfujg

--- src/battlezone/main/HUD.java
+++ src/battlezone/main/HUD.java
@@ -1,15 +1,19 @@
 package battlezone.main;
 import java.awt.*;
 
+import static battlezone.main.MainLogic.*;
+
 public class HUD {
 
 	Font font = new Font("Sans Serif", Font.PLAIN, 30);
 
 	public void render(Graphics2D graphics2D) {
-		graphics2D.setColor(Color.WHITE);
+		graphics2D.setColor(Color.green);
 		graphics2D.fillRect(100, 100, 100, 100);
 		graphics2D.setFont(font);
-		graphics2D.drawString("abc", 300, 300);
+		graphics2D.drawString("Score: "+score, 100, 400);
+		graphics2D.drawString("Health: "+health, 100, 350);
+		graphics2D.drawString("Ammunition: "+magazine, 100, 300);
 	}
 
 	public void update() {
